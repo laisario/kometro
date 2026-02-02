80ad54544b17eeebd5bc8a9c4e35a2b0bcfa554c
Laisa Muito Linda <laisa.rioverde@gmail.com>
2026-02-02 11:03:40 -0300

    testing deploy

--- bef-landing-page/pages/_app.js
+++ bef-landing-page/pages/_app.js
@@ -1,4 +1,3 @@
-import config from "@config/config.json";
 import theme from "@config/theme.json";
 import Head from "next/head";
 import { useEffect, useState } from "react";
@@ -6,9 +5,6 @@ import { GoogleTagManager } from "@next/third-parties/google";
 import "styles/style.scss";
 
 const App = ({ Component, pageProps }) => {
-  // default theme setup
-
-  // import google font css
   const pf = theme.fonts.font_family.primary;
   const sf = theme.fonts.font_family.secondary;
   const [fontcss, setFontcss] = useState();
@@ -23,7 +19,6 @@ const App = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        {/* google font css */}
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -34,7 +29,6 @@ const App = ({ Component, pageProps }) => {
             __html: `${fontcss}`,
           }}
         />
-        {/* responsive meta */}
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=5"
