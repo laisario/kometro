800561f34b61f8abc8dd5f320b79c302c3aecdc3
Laisa Muito Linda <laisa.rioverde@gmail.com>
2026-06-18 16:28:19 -0300

    adicionei novas tags

--- bef-landing-page/pages/_document.js
+++ bef-landing-page/pages/_document.js
@@ -12,6 +12,15 @@ const Document = () => {
           sizes="32x32"
           href={favicon}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TPCXNHT6');`,
+          }}
+        />
         {/* theme meta */}
         <meta name="theme-name" content="next-boilerplate" />
         <meta name="msapplication-TileColor" content="#000000" />
@@ -30,10 +39,10 @@ const Document = () => {
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=AW-602405826"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TPCXNHT6"
             height="0"
             width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
+            style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
         <Main />
